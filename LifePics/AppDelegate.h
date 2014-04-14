85484a571d076577cbc92b07db5c1cade903e836
Gabriel Moraes <gabriel.moraes@callsp.inf.br>
2014-04-13 23:50:42 -0300

    Estrutura inicial

---
+++ LifePics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LifePics
+//
+//  Created by Gabriel Moraes on 13/04/14.
+//  Copyright (c) 2014 Gabriel Moraes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LifePics/Controller/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LifePics
+//
+//  Created by Gabriel Moraes on 13/04/14.
+//  Copyright (c) 2014 Gabriel Moraes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
